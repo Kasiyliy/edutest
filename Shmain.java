48f97475aed31819100506637bb99a96c3b5e292
madinaAbd <abdulgafarova.madina@mail.ru>
2024-08-13 11:29:27 +0500

    Madina changes

--- Shmain.java
+++ Shmain.java
@@ -2,5 +2,6 @@ public class Shmain {
     public static void main(String[] args) {
         System.out.println("ASD");
         System.out.println("ASDASD");
+        System.out.println("ASDASDASD");
     }
 }
